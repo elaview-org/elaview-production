8bb327d5e5b555f0725d1770b8d517de59d8f8fc
chopsticks-user <frostyfrost273@gmail.com>
2026-01-04 17:36:13 -0500

    Login, Signup and themes

--- clients/web/shared/components/theme-toggle.tsx
+++ clients/web/shared/components/theme-toggle.tsx
@@ -1,26 +1,32 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import {Monitor, Moon, Sun} from "lucide-react"
-import {useTheme} from "next-themes"
+import * as React from "react";
+import {Monitor, Moon, Sun} from "lucide-react";
+import {useTheme} from "next-themes";
 
-import {Button} from "@/shared/components/ui/button"
+import {Button} from "@/shared/components/ui/button";
 import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
     DropdownMenuTrigger,
-} from "@/shared/components/ui/dropdown-menu"
+} from "@/shared/components/ui/dropdown-menu";
 
 export function ThemeToggle() {
-    const {setTheme} = useTheme();
+    const {setTheme, theme} = useTheme();
+    const [mounted, setMounted] = React.useState(false);
+
+    React.useEffect(() => {
+        setMounted(true);
+    }, []);
 
     return <DropdownMenu>
         <DropdownMenuTrigger asChild>
             <Button variant="ghost" size="sm">
-                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
-                <Moon
-                    className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
+                {!mounted && <Monitor className="h-[1.2rem] w-[1.2rem]"/>}
+                {mounted && theme === "system" && <Monitor className="h-[1.2rem] w-[1.2rem]"/>}
+                {mounted && theme === "light" && <Sun className="h-[1.2rem] w-[1.2rem]"/>}
+                {mounted && theme === "dark" && <Moon className="h-[1.2rem] w-[1.2rem]"/>}
                 <span className="sr-only">Toggle theme</span>
             </Button>
         </DropdownMenuTrigger>

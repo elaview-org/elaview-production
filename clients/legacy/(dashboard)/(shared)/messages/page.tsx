c1e1dee805fb7b5b26483d35223d6eced8785958
chopsticks-user <frostyfrost273@gmail.com>
2026-01-04 20:12:14 -0500

    Removed legacy

--- clients/legacy/(dashboard)/(shared)/messages/page.tsx
+++
@@ -1,8 +0,0 @@
-// src/app/(shared)/messages/page.tsx
-
-import { UnifiedMessagesLayout } from "@/shared/components/messages/UnifiedMessagesLayout";
-
-
-export default function MessagesPage() {
-  return <UnifiedMessagesLayout />;
-}
--- clients/legacy/(home)/page.tsx
+++
@@ -1,5 +0,0 @@
-import LandingPage from "@/shared/components/landing/LandingPage";
-
-export default function HomePage() {
-  return <LandingPage />;
-}

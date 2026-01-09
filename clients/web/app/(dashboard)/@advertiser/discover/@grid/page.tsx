99112a84bf5ef228d5b6a090bd67d886d90bd649
chopsticks-user <frostyfrost273@gmail.com>
2026-01-09 10:05:51 -0500

    [web] Added dialog component

--- clients/web/app/(dashboard)/@advertiser/discover/@grid/page.tsx
+++ clients/web/app/(dashboard)/@advertiser/discover/@grid/page.tsx
@@ -1,18 +1,5 @@
 import SpaceCard from "./space-card";
-
-import { Button } from "@/components/button";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/dialog";
-import { Input } from "@/components/input";
-import { Label } from "@/components/label";
+import { Dialog } from "@/components/dialog";
 
 const billboards = [
   {
@@ -190,40 +177,7 @@ export default function Page() {
     <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {billboards.map((billboard) => (
         <Dialog key={billboard.id}>
-          <form>
-            <DialogTrigger asChild>
-              <SpaceCard {...billboard} />
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-106.25">
-              <DialogHeader>
-                <DialogTitle>Edit profile</DialogTitle>
-                <DialogDescription>
-                  Make changes to your profile here. Click save when you&apos;re
-                  done.
-                </DialogDescription>
-              </DialogHeader>
-              <div className="grid gap-4">
-                <div className="grid gap-3">
-                  <Label htmlFor="name-1">Name</Label>
-                  <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
-                </div>
-                <div className="grid gap-3">
-                  <Label htmlFor="username-1">Username</Label>
-                  <Input
-                    id="username-1"
-                    name="username"
-                    defaultValue="@peduarte"
-                  />
-                </div>
-              </div>
-              <DialogFooter>
-                <DialogClose asChild>
-                  <Button variant="outline">Cancel</Button>
-                </DialogClose>
-                <Button type="submit">Save changes</Button>
-              </DialogFooter>
-            </DialogContent>
-          </form>
+          <SpaceCard {...billboard} />
         </Dialog>
       ))}
     </div>

0be94f53503caeba4080db0c5c3ff4b32a530967
chopsticks-user <frostyfrost273@gmail.com>
2026-01-12 20:58:11 -0500

    [DevOps] Added more secrets commands; Environment can now be passed to devbox

--- scripts/cmd/secrets.sh
+++ scripts/cmd/secrets.sh
@@ -10,15 +10,33 @@ ev_secrets_list() {
     doppler secrets
 }
 
+ev_secrets_get() {
+    ev_core_require_cmd "doppler" || return 1
+    doppler secrets get "$1" --plain
+}
+
+ev_secrets_set() {
+    ev_core_require_cmd "doppler" || return 1
+    doppler secrets set "$1=$2"
+}
+
+ev_secrets_notes() {
+    ev_core_require_cmd "doppler" || return 1
+    doppler secrets notes set "$1" "$2"
+}
+
 ev_secrets_dispatch() {
     cmd="$1"
     shift
 
     case "$cmd" in
         list) ev_secrets_list ;;
+        get) ev_secrets_get "$@" ;;
+        set) ev_secrets_set "$@" ;;
+        notes) ev_secrets_notes "$@" ;;
         *)
             ev_core_log_error "Unknown secrets command: $cmd"
-            echo "Available: list"
+            echo "Available: list, get, set, notes"
             return 1
             ;;
     esac
